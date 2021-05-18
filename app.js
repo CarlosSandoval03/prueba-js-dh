f2aa1639d6eb3b157e8636d90656dbf3ddcaa6fa
CarlosSandoval03 <carlos31sandoval98@gmail.com>
2021-05-17 21:17:18 -0500

    Commit 1: Examen terminado

--- app.js
+++ app.js
@@ -6,22 +6,39 @@
            * pendiente
          )
 */
+path = "tareas.json";
 
-exports.leerTareas = function () {
+let fs = require('fs');
+
+let leerTareas = function () {
     /*
         En esta función debes leer y retornar las tareas registradas.
     */
+   let tareas = fs.readFileSync(path,'utf-8');
+   return JSON.parse(tareas);
 }
+exports.leerTareas = leerTareas;
 
-exports.agregarTarea = function (tarea) {
+let agregarTarea = function (tarea) {
     /*
         Registra y guarda una nueva tarea.
     */
+   let tareas = leerTareas();
+   tareas.push(tarea);
+   let jsonConvert = JSON.stringify(tareas);
+   fs.writeFileSync(path,jsonConvert,'utf-8');
 }
+exports.agregarTarea = agregarTarea;
 
-exports.filtrarTareasPorEstado = function (estado) {
+let filtrarTareasPorEstado = function (estado) {
     /*
         En esta función debes de leer las tareas y retornar las que tengan el estado que se
         manda en el parametro.
     */
+   let tareas = leerTareas();
+   let filtradas = tareas.filter(function(tarea) {
+       return tarea.status==estado
+   })
+   return filtradas
 }
+exports.filtrarTareasPorEstado = filtrarTareasPorEstado;
